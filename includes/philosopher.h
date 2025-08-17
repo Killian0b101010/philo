d58e69aab7a21c82e0b7bc1d9374dd20339b0add
Killianb0101010 <teixeira.killian44600@gmail.com>
2025-08-17 19:45:12 +0200

    Push add init_table je dois decouper la fonction

--- includes/philosopher.h
+++ includes/philosopher.h
@@ -6,23 +6,30 @@
 /*   By: kiteixei <kiteixei@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/13 14:56:59 by kiteixei          #+#    #+#             */
-/*   Updated: 2025/08/15 01:26:23 by kiteixei         ###   ########.fr       */
+/*   Updated: 2025/08/17 18:39:27 by kiteixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <unistd.h>
 
 #ifndef PHILOSOPHER_H
 # define PHILOSOPHER_H
-# include <pthread.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>\
+# ifndef ERROR
+#  define ERROR "Paramaters invalid\n"
+# endif
+# ifndef END_MSG
+#  define END_MSG "Le temps est depasser\n"
+# endif
 
 typedef struct s_args
 {
 	int				nb_philo;
 	int				time_dead;
 	int				time_sleep;
-  int         time_eat;
+	int				time_eat;
 }					t_args;
 
 typedef struct s_philo
@@ -30,9 +37,26 @@ typedef struct s_philo
 	int				id;
 	struct s_philo	*next;
 	int				count_philo;
-  pthread_t thread;
+	int				last_meal;
+	int				meals_eaten;
+	pthread_mutex_t	*fork_g;
+	pthread_mutex_t	*fork_d;
+	pthread_t		thread;
+	int				i_d;
 	pthread_mutex_t	fork;
+	struct s_table	*table;
 }					t_philo;
 
-
+typedef struct s_table
+{
+	t_philo			*head;
+	int				count_live_philo;
+	int				count_live_eat;
+	int				start;
+	int				stop;
+	int				print_mutex;
+	int				stop_mutex;
+	int				now;
+	t_args			*args;
+}					t_table;
 #endif
--- src/philosopher.c
+++ src/philosopher.c
@@ -6,87 +6,129 @@
 /*   By: kiteixei <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 19:42:19 by kiteixei          #+#    #+#             */
-/*   Updated: 2025/08/14 19:56:19 by kiteixei         ###   ########.fr       */
+/*   Updated: 2025/08/17 19:39:29 by kiteixei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philosopher.h"
-/*
-void ft_convert_args(t_args *args)
+
+void	ft_convert_args(t_args *args, char **argv)
 {
-  args->nb_philo = ft_atoi;
-  args->time_dead =ft_atoi;
-  args->time_sleep = ft_atoi;
+	args->nb_philo = atol(argv[1]);
+	if (args->nb_philo <= 0)
+		return ;
+	args->time_dead = atol(argv[2]);
+	args->time_eat = atol(argv[3]);
+	args->time_sleep = atol(argv[4]);
 }
 
-void init_args(t_args *args)
-{  
-  argv[2][0] = args ->nb_philo;
-  if(args->nb_philo <= 0)
-      return;
-  argv[3][0] = args ->time_dead;
-  argv[4][0] = args -> time_sleep;
+void	*safe_malloc(size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(size);
+	if (!ptr)
+		return (NULL);
+	return (ptr);
 }
 
-void create_thread(t_args *args)
+void	free_philo_no_circle(t_philo *philo)
 {
-  int i = 0;
-  while(i < args -> nb_philo)
-  {
-    
-  }
+	t_philo	*head;
+	t_philo	*tmp;
+
+	head = philo;
+	while (head != NULL)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+	return ;
 }
+
+void	init_data(t_philo *philo, t_table *table, t_philo *head)
+{
+	philo->last_meal = table->start;
+	philo->fork_g = &philo->fork;
+	philo->table = table;
+	philo->meals_eaten = 0;
+	return ;
+}
+
+t_philo	*init_table(t_args *args, t_table *table)
+{
+	int		i;
+	t_philo	*head;
+	t_philo	*current;
+	t_philo	*tmp;
+	int		j;
+
+	head = safe_malloc(sizeof(t_philo));
+	if (!head)
+		return (NULL);
+	pthread_mutex_init(&head->fork, NULL);
+	current = head;
+	// cas ou 1 philo on init head;
+	if (args->nb_philo == 1)
+	{
+		current->next = head;
+		pthread_mutex_init(&current->fork, NULL);
+		head->id = 1;
+		init_data(head, table, head);
+		return (head);
+	}
+	// cas ou plusieurs philo
+	pthread_mutex_init(&current->fork, NULL);
+	head->id = 1;
+	init_data(head, table, head);
+	i = 2; // init a 2 car philo 1 gerer par defaut
+			// Plusieurs philo
+	while (i <= args->nb_philo)
+	{
+		current->next = safe_malloc(sizeof(t_philo));
+		if (!current->next)
+		{
+			free_philo_no_circle(head);
+			return (NULL);
+		}
+		current = current->next;
+		pthread_mutex_init(&current->fork, NULL);
+		current->id = i;
+		init_data(current, table, head);
+		i++;
+	}
+	current->next = head;
+	tmp = head;
+	j = 0;
+	// distrib des fourchettes
+	while (j < args->nb_philo)
+	{
+		tmp->fork_d = &tmp->next->fork;
+		tmp = tmp->next;
+		j++;
+	}
+	return (head);
+}
+
+void	manage_table(t_args *args, char **argv, t_table *table)
+{
+	t_philo	*philo;
+
+	ft_convert_args(args, argv);
+	philo = init_table(args, table);
+}
+
 int	main(int ac, char **argv)
 {
-  t_args args;
-  if(ac < 3)
-    return((write(2,"Error\n.",6),1);
-  ft_convert_args(args);
-  init_args(args);
+	t_args			args;
+	t_table			table;
+	struct timeval	current_time;
 
-
-}
-*/
-
-#include <pthread.h>
-#include <stdio.h>
-#include <unistd.h>
-
-void *routine1(void *arg) 
-{
-  int counter1 = 0;
- 
-    while (1) {
-        counter1++;
-        printf("Thread 1: counter1 = %d\n", counter1);
-        usleep(100000);
-    }
-    (void)arg;
-    return NULL;
-}
-
-void *routine2(void *arg) 
-{
-  int counter2 = 0;
-  (void)arg;
-    while (1) {
-        counter2++;
-        printf("Thread 2: counter2 = %d\n", counter2);
-        usleep(100000); 
-    }
-    
-    (void) arg;
-    return NULL;
-}
-
-int main() {
-    pthread_t t1, t2;
-
-    pthread_create(&t1, NULL, routine1, NULL);
-    pthread_create(&t2, NULL, routine2, NULL);
-
-    pthread_join(t1, NULL);
-    pthread_join(t2, NULL);
-
-    return 0;
+	gettimeofday(&current_time, NULL);
+	table.start = current_time.tv_sec * 1000 + current_time.tv_usec / 1000;
+	(void)argv;
+	if (ac < 3)
+		return (write(2, ERROR, 1));
+	manage_table(&args, argv, &table);
 }
